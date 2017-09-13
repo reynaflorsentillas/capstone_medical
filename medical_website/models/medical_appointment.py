3f79b419f49ce606e308b6b0a19b6ba2afd3f58d
reynaflorsentillas <reynaflorsentillas@ymail.com>
2017-09-13 18:28:00 +0800

    ============================ UPDATED: 09/13/2017 MODULE: Medical Base ============================ 1. Medical Website Module 	- Extend appoinment in medical_base: 		- Create patient button 		- Create patient function 		- Only display new patient field in form if patient is new, else hide it and show patient selection.

--- medical_website/models/medical_appointment.py
+++ medical_website/models/medical_appointment.py
@@ -1,8 +1,21 @@
 from odoo import api, models, fields, _
 
+import logging
+_logger = logging.getLogger(__name__)
+
 class MedicalAppointment(models.Model):
     _inherit = 'medical.appointment'
 
     # Appointment Online
     is_new_patient = fields.Boolean(string='Is New Patient?')
     new_patient = fields.Char(string='New Patient')
+
+    @api.multi
+    def action_create_patient(self):
+        for record in self:
+            Patient = self.env['medical.patient'].create({
+                'name': record.new_patient,
+                'email': record.patient_email,
+                'phone': record.patient_phone
+            })
+            record.write({'patient_id': Patient.id, 'is_new_patient': False})
